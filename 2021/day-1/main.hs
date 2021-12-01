7dcd9c4d6494bab3efb032418652b8b25aef5088
dgehringer <dgehringer@protonmail.com>
2021-12-01 12:56:35 +0100

    Remove unsued import statement

--- 2021/day-1/main.hs
+++ 2021/day-1/main.hs
@@ -1,6 +1,4 @@
 
-import Control.Monad (liftM)
-import Distribution.ParseUtils (fName)
 
 readInt :: String -> Int
 readInt = read
@@ -16,11 +14,10 @@ windowed s l =
                     take s l : windowed s xs
                 else windowed s xs
 
-numIncreasingMesurements :: Int ->[Int] -> Int
-numIncreasingMesurements ws numbers = (sum . map fromEnum) (zipWith (<) (init average) (tail average))
+numIncreasingMeasurements :: Int ->[Int] -> Int
+numIncreasingMeasurements ws numbers = (sum . map fromEnum) (zipWith (<) (init average) (tail average))
     where average = map sum (windowed ws numbers)
 
-problem1 = do fmap (numIncreasingMesurements 1) (readIntList "input.txt")
+problem1 = do fmap (numIncreasingMeasurements 1) (readIntList "input.txt")
 
-
-problem2 = do fmap (numIncreasingMesurements 3) (readIntList "input.txt")
+problem2 = do fmap (numIncreasingMeasurements 3) (readIntList "input.txt")
