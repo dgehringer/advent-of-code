994dad224ae2f1249ad71cf008ceb11b19e0c35b
dgehringer <dgehringer@protonmail.com>
2021-12-20 12:59:29 +0100

    Added day-20

--- 2021/day-20/main.jl
+++ 2021/day-20/main.jl
@@ -9,30 +9,31 @@ flat(A) = [A...]
 
 to_dec(arr) = foldl(((acc, d) -> acc*2 + (d == '#' ? 1 : 0)), arr; init=0)
 
-function grow_image(image)
+flip(c) = c == '#' ? '.' : '#'
+
+function grow_image(image, bg)
     (h, w) = size(image)
-    grown = fill('.', (h+4, w+4))
+    grown = fill(bg, (h+4, w+4))
     grown[3:(2+h), 3:(2+w)] = image
     (h,w), grown
 end
 
-function simulate(algo, image)
-    (h, w), gr = grow_image(image)
-    new = fill('.', (h+2, w+2))
-    for dh in 1:(h+2)
-        for dw in 1:(w+2)
-            @inbounds new[dh, dw] = algo[to_dec(flat(gr[dh:(dh+2), dw:(dw+2)]))+1]
-        end
-    end
-    new
+function simulate(algo, image, bg='.', flipbg=false)
+    (h, w), gr = grow_image(image, bg)
+    (flipbg ? flip(bg) : bg, [algo[to_dec(flat(gr[dh:(dh+2), dw:(dw+2)]))+1] for dh in 1:(h+2),  dw in 1:(w+2)])
 end
 
 (algo, img) = read_input()
 
-advance(image, i) = simulate(algo, image)
+flipbg = algo[1] == '#' && algo[end] == '.'
 
-final = foldl(advance, 1:2; init=img)
+function advance(d, i)
+    (bg, image) = d
+    simulate(algo, image, bg, flipbg)
+end
+
+(_, final) = foldl(advance, 1:2; init=('.', img))
 println("Part 1: $(count(==('#'), final))")
 
-final = foldl(advance, 1:50; init=img)
+(_, final) = foldl(advance, 1:50; init=('.', img))
 println("Part 2: $(count(==('#'), final))")
