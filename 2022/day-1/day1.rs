0ec9bc47309f11cafa6ac4c2fadb9702390e03e9
Dominik Gehringer <dgehringer@protonmail.com>
2022-12-01 09:26:40 +0100

    :art: improved day-1

--- 2022/day-1/day1.rs
+++ 2022/day-1/day1.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn parse_input(content: &str) -> Vec<Vec<usize>>{
+fn parse_input(content: &str) -> Vec<usize>{
     content
         .split("\n\n")
         .map(|elf_block|{
@@ -8,25 +8,23 @@ fn parse_input(content: &str) -> Vec<Vec<usize>>{
                 .split('\n')
                 .filter(|l| !l.is_empty())
                 .map(|l| l.parse::<usize>().expect("Failed to parse calories"))
-                .collect::<Vec<usize>>()
+                .sum::<usize>()
         })
         .collect()
 }
 
-fn part_one(calories: &[Vec<usize>]) {
-    let max_calories = calories.iter().map(|elf| elf.iter().sum::<usize>()).max().unwrap();
-    println!("Part1: {:?}", max_calories);
+fn part_one(calories_per_elf: &[usize]) {
+    println!("Part1: {:?}", calories_per_elf.iter().max().unwrap());
 }
 
-fn part_two(calories: &[Vec<usize>]) {
-    let mut calories_per_elf: Vec<usize> = calories.iter().map(|elf| elf.iter().sum::<usize>()).collect();
-    calories_per_elf.sort();
+fn part_two(calories_per_elf: &[usize]) {
     println!("Part2: {:?}", calories_per_elf.iter().rev().take(3).sum::<usize>());
 }
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Could not read puzzle input");
-    let calories = parse_input(&contents);
-    part_one(&calories);
-    part_two(&calories);
+    let mut calories_per_elf = parse_input(&contents);
+    calories_per_elf.sort();
+    part_one(&calories_per_elf);
+    part_two(&calories_per_elf);
 }
