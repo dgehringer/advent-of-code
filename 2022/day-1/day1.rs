909a2b957e002f97ad1fc3fe4bc88c67b2bea41d
Dominik Gehringer <dgehringer@protonmail.com>
2022-12-01 14:20:19 +0100

    :art: remove input downloading

--- 2022/day-1/day1.rs
+++ 2022/day-1/day1.rs
@@ -1,11 +1,8 @@
-mod helpers;
 
-use crate::helpers::fetch_input;
-
-fn parse_input(content: &str) -> Vec<usize>{
+fn parse_input(content: &str) -> Vec<usize> {
     content
         .split("\n\n")
-        .map(|elf_block|{
+        .map(|elf_block| {
             elf_block
                 .split('\n')
                 .filter_map(|l| l.parse::<usize>().ok())
@@ -19,11 +16,14 @@ fn part_one(calories_per_elf: &[usize]) {
 }
 
 fn part_two(calories_per_elf: &[usize]) {
-    println!("Part2: {:?}", calories_per_elf.iter().rev().take(3).sum::<usize>());
+    println!(
+        "Part2: {:?}",
+        calories_per_elf.iter().rev().take(3).sum::<usize>()
+    );
 }
 
 fn main() {
-    let contents = fetch_input(2022, 1).expect("Could not load input");
+    let contents = std::fs::read_to_string("input.txt").expect("Failed to read input");
     let mut calories_per_elf = parse_input(&contents);
     calories_per_elf.sort();
     part_one(&calories_per_elf);
