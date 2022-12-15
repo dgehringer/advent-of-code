b532f7eb0791a137db5e8483d930c8ab48da1680
Dominik Gehringer <dgehringer@protonmail.com>
2022-12-15 10:38:25 +0100

    :recycle: format black day 15

--- 2022/day-15/day15.py
+++ 2022/day-15/day15.py
@@ -1,4 +1,3 @@
-
 import re
 from multiprocessing import Pool, cpu_count
 from typing import *
@@ -6,13 +5,16 @@ from typing import *
 Point = Beacon = Sensor = Tuple[int, int]
 Grid = List[Tuple[Beacon, Sensor]]
 
+
 def parse_input(inp: str) -> Grid:
-    coord_regex = re.compile(r'[x|y]=([+-]?\d+)')
+    coord_regex = re.compile(r"[x|y]=([+-]?\d+)")
+
     def parse_line(l: str) -> Tuple[Sensor, Beacon]:
         srow, scol, brow, bcol = map(int, coord_regex.findall(l))
         return (srow, scol), (brow, bcol)
 
     return list(map(parse_line, inp.splitlines()))
+
 
 def dist(a: Point, b: Point) -> int:
     ar, ac = a
@@ -42,13 +44,15 @@ def intervals_for_row(grid: Grid, target: int = 2000000) -> List[Point]:
 
     return merged_intervals
 
-def chunks(rmin: int, rmax: int, chunk_size: Optional[int] = None) -> Iterable[Iterable[int]]:
+
+def chunks(
+    rmin: int, rmax: int, chunk_size: Optional[int] = None
+) -> Iterable[Iterable[int]]:
     chunk_size = chunk_size or abs(rmax - rmin) // cpu_count()
     if abs(rmax - rmin) % chunk_size != 0:
         raise ValueError
-    indices = list(range(rmin, rmax+1, chunk_size))
+    indices = list(range(rmin, rmax + 1, chunk_size))
     return [range(*r) for r in zip(indices[:-1], indices[1:])]
-
 
 
 def find_freq(grid: Grid, rows: Iterable[int], max_row: int) -> Optional[int]:
@@ -56,7 +60,7 @@ def find_freq(grid: Grid, rows: Iterable[int], max_row: int) -> Optional[int]:
         intervals = intervals_for_row(grid, row)
         if len(intervals) > 1:
             _, col = intervals[0]
-            return (col +1)* max_row + row
+            return (col + 1) * max_row + row
     return None
 
 
@@ -67,14 +71,15 @@ def part_two(grid: Grid, max_row: int = 4000000):
         for freq in pool.starmap(find_freq, inputs, chunksize=1):
             if freq is not None:
                 result = freq
-    print('Part 2:', result)
+    print("Part 2:", result)
 
 
 def part_one(grid: Grid, target: int = 2000000):
     merge_col, merge_row = next(iter(intervals_for_row(grid, target)))
-    print('Part 1:',  merge_row - merge_col)
+    print("Part 1:", merge_row - merge_col)
 
-with open('input.txt') as h:
+
+with open("input.txt") as h:
     contents = h.read()
 
 board = parse_input(contents)
