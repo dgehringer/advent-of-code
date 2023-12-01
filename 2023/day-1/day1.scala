e5a779f0a89266501b18aa5dfca1c7b364c07219
dgehringer <{ID}+{username}@users.noreply.github.com>
2023-12-01 12:36:17 +0100

    add day 1

---
+++ 2023/day-1/day1.scala
@@ -0,0 +1,23 @@
+import scala.io.Source
+
+val lines = Source.fromResource("input.txt").getLines.toList
+
+def firstAndLastDigit(digits: Iterable[Int]): Int =
+  if digits.isEmpty then 0
+  else digits.head * 10 + digits.last
+
+
+val partOne = lines.map(line =>
+  firstAndLastDigit(line.flatMap(_.toString.toIntOption))
+).sum
+
+val digitNames = List("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+val digits =
+  (digitNames zip (1 to digitNames.length)).toMap ++ ((1 to 9).map(d => (d.toString, d))).toMap
+
+
+def findDigits(line: String): Vector[Int] =
+  def findDigitAt(pos: Int) = digits.keySet.find(line.startsWith(_, pos))
+  (line.indices flatMap findDigitAt map digits).toVector
+
+val partTwo = (lines map (findDigits andThen firstAndLastDigit)).sum
