b35379575bce9f3e1779fb0b80aab6814428a350
dgehringer <35268573+dgehringer@users.noreply.github.com>
2025-12-04 22:52:23 +0100

    add day2

--- 2025/src/day2.rs
+++ 2025/src/day2.rs
@@ -1,58 +1,68 @@
-
 mod input;
 
+use crate::input::load_input;
+use itertools::Itertools;
 use std::fs;
 use std::ops::Range;
-use crate::input::load_input;
 
 type Instruction = (char, isize);
 
-fn parse_input(input: &str) -> Vec<Range<usize>> {
-    input.lines().next().unwrap().split(", ").filter_map(|part| {
-        let mut split = part.split('-');
-        let start = split.next()?.parse::<usize>().ok()?;
-        let end = split.next()?.parse::<usize>().ok()?;
-        Some(start..end)
-    }).collect()
+fn parse_input(input: &str) -> Vec<(usize, usize)> {
+    input
+        .lines()
+        .next()
+        .unwrap()
+        .split(",")
+        .filter_map(|part| {
+            let mut split = part.split('-');
+            let start = split.next()?.parse::<usize>().ok()?;
+            let end = split.next()?.parse::<usize>().ok()?;
+            Some((start, end))
+        })
+        .collect()
 }
 
-fn part1(instructions: &[Instruction]) -> isize {
-    let mut clicks = 0isize;
-    let mut dial = 50isize;
-    for (char, amount) in instructions {
-        dial = match char {
-            'L' => (dial - amount),
-            'R' => (dial + amount),
-            _ => unimplemented!(),
-        } % 100;
-        if dial < 0 {
-            dial += 100;
-        }
-        if dial == 0 {
-            clicks += 1;
-        }
-    }
-    clicks
+fn part1(ranges: &[(usize, usize)]) -> usize {
+    ranges
+        .iter()
+        .flat_map(|&(start, end)| {
+            (start..=end).filter(|v| {
+                if v.ilog10() % 2 == 1 {
+                    let v_str = &v.to_string();
+                    let (left, right) = v_str.split_at(v_str.len() / 2);
+                    left == right
+                } else {
+                    false
+                }
+            })
+        })
+        .sum()
 }
 
-fn part2(instructions: &[Instruction]) -> isize {
-    let mut clicks = 0isize;
-    let dial = 50isize;
-    for (char, amount) in instructions {
-        clicks += match char {
-            'L' => dial+1..=dial+amount,
-            'R' => dial-1..=dial-amount,
-            _ => unimplemented!(),
-        }.filter(|d| d % 100 == 0).count() as isize
-    }
-    clicks * 2 - 1
+fn part2(ranges: &[(usize, usize)]) -> usize {
+    ranges
+        .iter()
+        .flat_map(|&(start, end)| {
+            (start..=end).filter(|v| {
+                let v_str = v.to_string();
+                (1..=v_str.len() / 2).any(|i| {
+                    let chunks: Vec<String> = v_str
+                        .chars()
+                        .chunks(i)
+                        .into_iter()
+                        .map(|chunk| chunk.collect())
+                        .collect();
+                    let first = &chunks[0];
+                    chunks.iter().all(|c| c == first)
+                })
+            })
+        })
+        .sum()
 }
 
 fn main() {
     let input = load_input(2025, 2, None);
-    let input = fs::read_to_string("test").unwrap();
     let ranges = parse_input(&input);
-    println!("Part 1: {:?}", ranges);
-
-
+    println!("Part 1: {:?}", part1(&ranges));
+    println!("Part 1: {:?}", part2(&ranges));
 }
